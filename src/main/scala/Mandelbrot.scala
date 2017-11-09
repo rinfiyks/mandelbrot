b397c01b005914dfb34f557c8d9fa005e2985e51
rinfiyks <tom.gould.92@gmail.com>
2017-11-09 01:46:06 +0000

    Write down some refactoring thoughts in comments

--- src/main/scala/Mandelbrot.scala
+++ src/main/scala/Mandelbrot.scala
@@ -4,6 +4,16 @@ case class RGBColour(r: Int, g: Int, b: Int) {
   def toScalaFXColor: Color = Color.rgb(r, g, b)
 }
 
+case class Pixel(x: Int, y: Int)
+
+/*
+MCanvas needs renaming, and should probably be a list/array of (Pixel, Complex)
+the code to work out the colour should be elsewhere.
+1. what should a class of (Pixel, Complex) be called?
+  ComplexPoint?
+2. and what is a list of them called?
+  lattice/grid/section...
+*/
 case class MCanvas(pixels: Array[Array[RGBColour]])
 
 object MCanvas {
@@ -13,7 +23,7 @@ object MCanvas {
     val matrix = Array.tabulate[RGBColour](width, height) { (w, h) =>
       val x = centreX + zoom * (w.toDouble / width - 0.5)
       val y = centreY + zoom * (h.toDouble / height - 0.5) * height.toDouble / width
-      getColour(Complex(x, y), maxIterations)
+      getColour(Complex(x, y), maxIterations) // bad - getColour shouldn't be coupled into this class
     }
 
     MCanvas(matrix)
@@ -24,7 +34,7 @@ object MCanvas {
 
     @annotation.tailrec
     def go(z: Complex = Complex(0, 0), iteration: Int = 0): RGBColour = {
-      if (iteration > maxIterations) return RGBColour(0, 0, 0)
+      if (iteration > maxIterations) return RGBColour(0, 0, 0) // Black
       val next: Complex = (z * z) + c
       if (next.absSquared > 4) {
         val r = iteration % maxColour
